44d502db53e96146199cfd2de9af6743e8f25735
Ezki15 <110952841+Ezki15@users.noreply.github.com>
2025-05-31 17:15:28 +0800

    feature replies: ReplyRepositoryPostgres implemented

---
+++ src/Infrastructures/repository/ReplyRepositoryPostgres.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
+const SavedReply = require('../../Domains/replies/entities/SavedReply');
+const ReplyRepository = require('../../Domains/replies/ReplyRepository');
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+
+class ReplyRepositoryPostgres extends ReplyRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addReply(newReply) {
+    const { content, owner, commentId } = newReply;
+    const id = `reply-${this._idGenerator()}`;
+    const is_deleted = 'tidak';
+    const create_at = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO replies VALUES($1, $2, $3, $4, $5, $6) RETURNING id, content, owner',
+      values: [id, content, owner, is_deleted, create_at, commentId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return new SavedReply({ ...result.rows[0] });
+  }
+
+  async verifyReplyOwner(replyId, owner) {
+    const query = {
+      text: 'SELECT * FROM replies WHERE id = $1',
+      values: [replyId],
+    };
+
+    const result = await this._pool.query(query);
+    const reply = result.rows[0];
+    if (reply.owner !== owner) {
+      throw new AuthorizationError('Tidak dapat mengakses sumber ini!');
+    }
+  }
+}
+
+module.exports = ReplyRepositoryPostgres;
---
+++ src/Infrastructures/repository/_test/ReplyRepositoryPostgres.test.js
@@ -0,0 +1,105 @@
+/* eslint-disable no-unused-vars */
+const RepliesTableTestHelper = require('../../../../tests/RepliesTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const NotFoundError = require('../../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../../Commons/exceptions/AuthorizationError');
+const NewReply = require('../../../Domains/replies/entities/NewReply');
+const SavedReply = require('../../../Domains/replies/entities/SavedReply');
+const pool = require('../../database/postgres/pool');
+const ReplyRepositoryPostgres = require('../ReplyRepositoryPostgres');
+
+describe('ReplyRepositoryPostgres', () => {
+  // add user data because reply need userId to fill owner
+  beforeEach(async () => {
+    await UsersTableTestHelper.addUser({});
+    await ThreadsTableTestHelper.addThread({});
+    await CommentsTableTestHelper.addComment({});
+  });
+
+  afterEach(async () => {
+    await RepliesTableTestHelper.cleanTable();
+    await CommentsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addReply function', () => {
+    it('should persist new reply and return saved reply correctly', async () => {
+      // Arrange
+      const credentialUser = 'user-123';
+      const commentId = 'comment-123';
+      const newReply = new NewReply({
+        content: 'Reply content',
+      }, credentialUser, commentId);
+      const fakeIdGenerator = () => '123'; // stub!
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      await replyRepositoryPostgres.addReply(newReply);
+
+      // Assert
+      const reply = await RepliesTableTestHelper.findReplyById('reply-123');
+      expect(reply).toHaveLength(1);
+    });
+
+    it('should return saved reply correctly', async () => {
+      // Arrange
+      const credentialUser = 'user-123';
+      const commentId = 'comment-123';
+      const newReply = new NewReply({
+        content: 'Reply content',
+      }, credentialUser, commentId);
+      const fakeIdGenerator = () => '123'; // stub!
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const savedReply = await replyRepositoryPostgres.addReply(newReply);
+
+      // Assert
+      expect(savedReply).toStrictEqual(new SavedReply({
+        id: 'reply-123',
+        content: 'Reply content',
+        owner: credentialUser,
+      }));
+    });
+  });
+
+  describe('verifyReplyOwner function', () => {
+    it('should throw AuthorizationError when user is not the owner', async () => {
+      // Arrange
+      const credentialUser = 'user-123';
+      const commentId = 'comment-123';
+      const newReply = new NewReply({
+        content: 'Reply content',
+      }, credentialUser, commentId);
+      const fakeIdGenerator = () => '123'; // stub!
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, fakeIdGenerator);
+      // Action
+      await replyRepositoryPostgres.addReply(newReply);
+      // Assert
+      await expect(replyRepositoryPostgres.verifyReplyOwner('reply-123', 'user-456'))
+        .rejects.toThrow(AuthorizationError);
+    });
+    it('should not throw AuthorizationError when user is the owner', async () => {
+      // Arrange
+      const credentialUser = 'user-123';
+      const commentId = 'comment-123';
+      const newReply = new NewReply({
+        content: 'Reply content',
+      }, credentialUser, commentId);
+      const fakeIdGenerator = () => '123'; // stub!
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, fakeIdGenerator);
+      // Action
+      await replyRepositoryPostgres.addReply(newReply);
+      // Assert
+      await expect(replyRepositoryPostgres.verifyReplyOwner('reply-123', credentialUser))
+        .resolves.not.toThrow(AuthorizationError);
+    });
+  });
+});
